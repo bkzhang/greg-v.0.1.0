9418452996de42e35a7acff392424c2b6959fa26
bkzhang <brian.k.zhang@gmail.com>
2017-02-20 17:53:36 -0500

    quicksave

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ const bot = new Discord.Client({
   owner: config.owner  
 })
 
+const prefix = '!'
+
 bot
   .on('error', console.error)
   .on('warn', console.warn)
@@ -27,16 +29,13 @@ bot
     console.warn('Reconnecting...slowly')
   })
   .on('message', (message) => {
+    if (message.author.bot) return
     if (Command.msg('good shit', message)) {
       message.channel.sendMessage(':ok_hand::eyes::ok_hand::eyes::ok_hand::eyes::ok_hand::eyes::ok_hand::eyes: good shit go?? sHit:ok_hand: thats :heavy_check_mark: some good:ok_hand::ok_hand:shit right:ok_hand::ok_hand:th :ok_hand: ere:ok_hand::ok_hand::ok_hand: right:heavy_check_mark:there :heavy_check_mark::heavy_check_mark:if i do??a? so?my self :100: i say so :100: thats what im talking about right there right there (chorus: ????? ?????) mMMMM??:100: :ok_hand::ok_hand: :ok_hand:?O0??OOOOO???Oooo?????????:ok_hand: :ok_hand::ok_hand: :ok_hand: :100: :ok_hand: :eyes: :eyes: :eyes: :ok_hand::ok_hand:Good shit')
     }
-  })
-  .on('message', (message) => {
-    if (message.author.username != 'shitposts') {
-      if (Command.msg('roll', message)) {
-        let roll = Math.floor(Math.random() * 20) + 1
-        message.reply('rolled a ' + roll)
-      }
+    if (Command.msg(prefix + 'roll', message)) {
+      let roll = Math.floor(Math.random() * 20) + 1
+      message.reply('rolled a ' + roll)
     }
   })
   .login(config.token)
